aa0e858fbc88a943404dcf8a045d742d484814c1
Andra Winarko <61117072+michaelcbnw@users.noreply.github.com>
2022-04-23 08:45:36 +0700

    Update RGBmatrixPanel.cpp
    Change: Constructor for 32x16 or 64x16 panel

--- RGBmatrixPanel.cpp
+++ RGBmatrixPanel.cpp
@@ -207,7 +207,8 @@ RGBmatrixPanel::RGBmatrixPanel(uint8_t a, uint8_t b, uint8_t c, uint8_t clk,
   );
 }
 
-// Constructor for 32x32 or 32x64 panel:
+
+// Constructor for 32x16 or 64x16 panel:
 RGBmatrixPanel::RGBmatrixPanel(uint8_t a, uint8_t b, uint8_t c, uint8_t d,
                                uint8_t clk, uint8_t lat, uint8_t oe,
                                boolean dbuf, uint8_t width
@@ -216,7 +217,7 @@ RGBmatrixPanel::RGBmatrixPanel(uint8_t a, uint8_t b, uint8_t c, uint8_t d,
                                uint8_t *pinlist
 #endif
                                )
-    : Adafruit_GFX(width, 32) {
+    : Adafruit_GFX(width, 16) {
 
   init(16, a, b, c, clk, lat, oe, dbuf, width
 #if defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_ESP32)
